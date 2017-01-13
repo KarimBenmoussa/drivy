f9e79b5ecf82f415ed533c16c32047ead191c028
Karim <karim.benmoussa33@gmail.com>
2017-01-13 19:00:33 +0100

    Calculate price

--- public/index.js
+++ public/index.js
@@ -32,6 +32,9 @@ var rentals = [{
   'pickupDate': '2016-01-02',
   'returnDate': '2016-01-02',
   'distance': 100,
+   'time' : diffDays(rentals[0].pickupDate, rentals[0].returnDate),
+
+  'price' : calculPrice('time', cars, rentals[0].id),
   'options': {
     'deductibleReduction': false
   },
@@ -51,6 +54,9 @@ var rentals = [{
   'pickupDate': '2016-01-05',
   'returnDate': '2016-01-09',
   'distance': 300,
+  'time' : diffDays('pickupDate', 'returnDate'),
+
+  'price' : calculPrice('time', cars, 'id'),
   'options': {
     'deductibleReduction': true
   },
@@ -70,6 +76,9 @@ var rentals = [{
   'pickupDate': '2015-12-01',
   'returnDate': '2015-12-15',
   'distance': 1000,
+  'time' : diffDays('pickupDate', 'returnDate'),
+
+  'price' : calculPrice('time', cars, 'id'),
   'options': {
     'deductibleReduction': true
   },
@@ -169,3 +178,87 @@ console.log(cars);
 console.log(rentals);
 console.log(actors);
 console.log(rentalModifications);
+/*
+function timeTotal(rentals){
+var diffDaysPerPerson = [];
+for(int i = 0; i < rentals.length; i++){
+  r = rentals[i].returnDate;
+  p = rentals[i].pickupDate;
+  var date1 = new Date(p);
+  var date2 = new Date(r);
+  var timeDiff = Math.abs(date2.getTime() - date1.getTime());
+  var diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24)); 
+  diffDaysPerPerson.push(diffDays);
+}
+  return diffDaysPerPerson;
+}
+*/
+/*
+function diffDays(rentals, i){
+  r = rentals[i].returnDate;
+  p = rentals[i].pickupDate;
+  var date1 = new Date(p);
+  var date2 = new Date(r);
+  var timeDiff = Math.abs(date2.getTime() - date1.getTime());
+  var diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24)); 
+  return diffDays;
+}
+*/
+function diffDays(p, r){  
+  var date1 = new Date(p);
+  var date2 = new Date(r);
+  var timeDiff = Math.abs(date2.getTime() - date1.getTime());
+  var diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24)); 
+  return diffDays;
+}
+/*
+function calculPrice(cars ,rentals, ){
+  allPrice = [];
+  for(int i = 0; i < rentals.length; i++){
+    
+    if(cars[i].id == rentals[i].carId)
+    {
+      time = diffDays(rentals, i);
+      priceDays = time * cars[i].pricePerDay;
+      distanceTotal = rentals[i].distance * cars[i].pricePerKm;
+      price = distanceTotal + priceDays;
+      allPrice.push(price);
+    }
+  }
+}
+
+function calculPrice(cars ,rentals, i){
+  allPrice = [];
+      
+    if(cars[i].id == rentals[i].carId)
+    {
+      time = diffDays(rentals, i);
+      priceDays = time * cars[i].pricePerDay;
+      distanceTotal = rentals[i].distance * cars[i].pricePerKm;
+      price = distanceTotal + priceDays;
+      allPrice.push(price);
+    }
+    for( i = 0; i < rentals.length; i++ ){
+      if(cars[0].id == rentals[i].carId){
+      time = diffDays(rentals, i);
+      priceDays = time * cars[i].pricePerDay;
+      distanceTotal = rentals[i].distance * cars[i].pricePerKm;
+      price = distanceTotal + priceDays;
+      }
+    }
+  return price;
+}
+*/
+function calculPrice(time ,cars, rentals){ 
+      for(i = 0; i < cars.length ; i++){       
+          if(cars[i].id == rentals){
+          priceDays = time * cars[i].pricePerDay;
+          distanceTotal = rentals.distance * cars[i].pricePerKm;
+          price = distanceTotal + priceDays; 
+          
+        }
+      }
+         
+  
+  return price;
+}
